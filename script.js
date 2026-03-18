50ec190e74d40812efc763d2dd6ac6b67cb5e9b3
Pradipta Sarker <techshuvo@gmail.com>
2026-03-18 16:27:25 +0600

    api fetch function add , many error , still thinking

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ console.log("Hello, World!");
 // my_modal_5.showModal();
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// grab all section
+// grab all Major section
 const header = document.querySelector("header");
 const loginSection = document.getElementById("login");
 const loginForm = document.querySelector("#login form");
 const mainSection = document.getElementById("maincontent");
 const footer = document.querySelector("footer");
+
+//subsection section for data manupulations
+
+const loader = document.getElementById("loader");
+const issueTab = document.getElementById("issue-tab");
+const issueCount = document.getElementById("issue-count");
+const issueListContainer = document.getElementById("issue-list-container");
 
 // Hide All Except Login
 // header.classList.add("hidden");
@@ -50,3 +57,108 @@ loginForm.addEventListener("submit", (event) => {
 });
 
 // fetch all data in the all tabs
+
+const allDataShow = async () => {
+  const allDataUrl = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
+
+  try {
+    const res = await fetch(allDataUrl);
+    const data = await res.json();
+    showIssueData(data);
+
+    loader.classList.add("hidden");
+  } catch (error) {}
+};
+
+allDataShow();
+
+const showIssueData = (res) => {
+  const issueList = res.data;
+  console.log(issueList);
+  //   id": 1,
+  // "title": "Fix navigation menu on mobile devices",
+  // "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+  // "status": "open",
+  // "labels": [
+  // "bug",
+  // "help wanted"
+  // ],
+  // "priority": "high",
+  // "author": "john_doe",
+  // "assignee": "jane_smith",
+  // "createdAt": "2024-01-15T10:30:00Z",
+  // "updatedAt": "2024-01-15T10:30:00Z"
+
+  issueListContainer.innerHTML = "";
+  issueCount.innerHTML = `${issueList.length} Issues `;
+
+  issueList.forEach((issue) => {
+    issue.innerHTML = `
+  <div
+                  class="single-issue-card border-t-5 border-t-green-500 card w-full bg-base-100 shadow-sm"
+                >
+                  <div class="card-body overflow-hidden">
+                    <div class="flex justify-between items-center">
+                      <img src="./assets/Open-Status.png" alt="" />
+                      <span class="uppercase badge badge-soft badge-secondary">${issue.priority}</span>
+                    </div>
+
+                    <div class="space-y-2">
+                      <h2 class="font-semibold text-lg">
+                        ${issue.title}
+                      </h2>
+                      <p class="text-gray-400">
+                        ${issue.description}
+                      </p>
+                      <div>
+                        <span
+                          class="uppercase badge badge-soft badge-secondary gap-1"
+                          ><img src="./assets/bug.svg" alt="" /></span
+                        >
+                        <span
+                          class="uppercase badge badge-soft badge- gap-1"
+                        >
+                          <img src="./assets/lifebouy.svg" alt="" />help
+                          wanted</span
+                        >
+                        ${issue.labels.map((label) => {
+                          label = label.toLowerCase();
+                          let badgeColor = "badge-error";
+                          if (label === "bug") {
+                            badgeColor = "badge-secondary";
+                          } else if (label === "help wanted") {
+                            badgeColor = "badge-warning";
+                          } else if (label === "enhancement") {
+                            badgeColor = "badge-accent";
+                          } else if (label === "good first issue") {
+                            badgeColor = "badge-success";
+                          } else {
+                            badgeColor = "badge-success";
+                          }
+                        })}
+                      </div>
+                    </div>
+                    <div class="divider my-0 gap-0 w-[130%] -ml-[15%]"></div>
+                    <div class="flex justify-between items-center">
+                      <span class="text-gray-400">#1 by John Doe</span>
+                      <span class="text-gray-400">1/15/2024</span>
+                    </div>
+                    <div class="flex justify-between items-center">
+                      <span class="text-gray-400"
+                        >Assignee:<br />
+                        <span class="font-semibold text-neutral-500"
+                          >john doe
+                        </span>
+                      </span>
+                      <span class="text-gray-400"
+                        >Updated:<br /><span
+                          class="font-semibold text-neutral-500"
+                          >1/15/2024</span
+                        ></span
+                      >
+                    </div>
+                  </div>
+                </div>
+  `;
+  });
+};
